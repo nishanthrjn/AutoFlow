e81690134ad6d1bbd04604b4e52cee89e44d6f87
Nishanth Rajan <nishanthrjn@gmail.com>
2026-04-30 22:15:17 +0200

    chore: initial solution setup with clean architecture layers

---
+++ src/AutoFlow.Engine/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace AutoFlow.Engine;
+
+public class Class1
+{
+
+}
---
+++ tests/AutoFlow.Engine.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoFlow.Engine.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
